d34a2d35a564bb24d4522b0d5aca1698240a446e
catmayo <macatherine48@gmail.com>
2025-12-03 12:51:42 -0800

    navbar more work

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const navLinks = document.querySelector('.nav-links');
   const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('nav-links');
 
-  // Mobile hamburger toggle
   hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('show');
   });
